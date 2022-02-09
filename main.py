69cd261a652563dd668f50d8ea26bad60de08fa4
Bhargava Krishna Marripati <marripatibhargava@gmail.com>
2022-02-09 19:44:02 +0530

    Initial Commit (Test) Updated till Python Numbers updated till place holders

--- main.py
+++ main.py
@@ -139,4 +139,111 @@ print(type(a))
 print(type(b))
 print(type(c))
 
+#Import the random module, and display a random number between 1 and 9:
+import random
+
+print(random.randrange(1, 10))
+
+#Get the character at position 1 (remember that the first character has the position 0):
+a = "Hello, World!"
+print(a[1])
+
+#Loop through the letters in the word "banana":
+for x in "banana":
+  print(x)
+
+#The len() function returns the length of a string:
+a = "Hello, World!"
+print(len(a))
+
+#Check if "free" is present in the following text:
+txt = "The best things in life are free!"
+print("free" in txt)
+
+#Print only if "free" is present:
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+#Check if "expensive" is NOT present in the following text:
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+#print only if "expensive" is NOT present:
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+#Get the characters from position 2 to position 5 (not included):
+b = "Hello, World!"
+print(b[2:5])
+
+#Get the characters from position 2, and all the way to the end:
+b = "Hello, World!"
+print(b[2:])
+
+#Get the characters:
+#From: "o" in "World!" (position -5)
+#To, but not included: "d" in "World!" (position -2):
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+#The upper() method returns the string in upper case:
+a = "Hello, World!"
+print(a.upper())
+
+#The lower() method returns the string in lower case:
+a = "Hello, World!"
+print(a.lower())
+
+#The strip() method removes any whitespace from the beginning or the end:
+a = " Hello, World! "
+print(a.strip())
+
+#The replace() method replaces a string with another string:
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+#The split() method splits the string into substrings if it finds instances of the separator:
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+
+#Python - String Concatenation
+#Merge variable a with variable b into variable c:
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+#Use the format() method to insert numbers into strings:
+
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+#The format() method takes unlimited number of arguments, and are placed into the respective placeholders:
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+
+#You can use index numbers {0} to be sure the arguments are placed in the correct placeholders:
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
+
+
+
+
+
+
+
+
+
 
