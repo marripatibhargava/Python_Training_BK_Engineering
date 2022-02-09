0ad68107502823a7dfb09eac6f4e54a7024d728d
Bhargava Krishna Marripati <marripatibhargava@gmail.com>
2022-02-09 15:10:45 +0530

    Initial Commit (Test) Updated till Python Numbers

--- main.py
+++ main.py
@@ -1,10 +1,142 @@
+# Initial Print - Tradition
+
+print("Hello, World!")
 
 #Python Indentation
 
 if 5 > 2:
   print("Five is greater than two!")
 
+  """
+  This is a comment
+  written in
+  more than just one line
+  """
 
+x = 4       # x is of type int
+y = "Sally" # x is now of type str
+print(x)
+print(y)
 
+# Casting - If you want to specify the data type of a variable, this can be done with casting.
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(x)
+print(y)
+print(z)
+
+# You can get the data type of a variable with the type() function.
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+"""
+Variable Names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+"""
+
+# Python allows you to assign values to multiple variables in one line:
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#One Value to Multiple Variables
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+#Unpack a Collection
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+#Output Variables
+x = "awesome"
+print("Python is " + x)
+
+x = "Python is "
+y = "awesome"
+z =  x + y
+print(z)
+
+#Global Variables
+
+#Create a variable outside of a function, and use it inside the function
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+#Create a variable inside a function, with the same name as the global variable
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+#If you use the global keyword, the variable belongs to the global scope:
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+#To change the value of a global variable inside a function, refer to the variable by using the global keyword:
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+#Print the data type of the variable x:
+x = 5
+print(type(x))
+
+#Convert from one data type to another:
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
 
 
