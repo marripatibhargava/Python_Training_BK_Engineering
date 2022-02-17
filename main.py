be72506342422cf1c08a3d2cc1cbf6874b9a6d61
Bhargava Krishna Marripati <marripatibhargava@gmail.com>
2022-02-17 14:47:11 +0530

    Initial Commit (Test) Updated till Python Numbers updated till place holders Updated till comprehension 591 tuples done 724 Sets Done

--- main.py
+++ main.py
@@ -592,17 +592,133 @@ print(mytuple)
 
 #Python Sets
 
+#Get the number of items in a set:
 
+thisset = {"apple", "banana", "cherry"}
 
+print(len(thisset))
 
+#String, int and boolean data types:
 
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
 
+#A set with strings, integers and boolean values:
 
+set1 = {"abc", 34, True, 40, "male"}
 
+#What is the data type of a set?
 
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
 
+#Using the set() constructor to make a set:
 
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
 
+#Loop through the set, and print the values:
 
+thisset = {"apple", "banana", "cherry"}
 
+for x in thisset:
+  print(x)
 
+#Check if "banana" is present in the set:
+
+thisset = {"apple", "banana", "cherry"}
+print("banana" in thisset)
+
+#Add an item to a set, using the add() method:
+
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+
+#Add elements of a list to at set:
+
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+thisset.update(mylist)
+print(thisset)
+
+Remove "banana" by using the remove() method:
+
+thisset = {"apple", "banana", "cherry"}
+thisset.remove("banana")
+print(thisset)
+
+#Remove "banana" by using the discard() method:
+
+thisset = {"apple", "banana", "cherry"}
+thisset.discard("banana")
+print(thisset)
+
+#Remove the last item by using the pop() method:
+
+thisset = {"apple", "banana", "cherry"}
+x = thisset.pop()
+print(x)
+print(thisset)
+
+#The clear() method empties the set:
+
+thisset = {"apple", "banana", "cherry"}
+thisset.clear()
+print(thisset)
+
+#The del keyword will delete the set completely:
+
+thisset = {"apple", "banana", "cherry"}
+del thisset
+print(thisset)
+
+#Loop through the set, and print the values:
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+#The union() method returns a new set with all items from both sets:
+
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+set3 = set1.union(set2)
+print(set3)
+
+#The update() method inserts the items in set2 into set1:
+
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+set1.update(set2)
+print(set1)
+
+#Keep the items that exist in both set x, and set y:
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+x.intersection_update(y)
+print(x)
+
+#Return a set that contains the items that exist in both set x, and set y:
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+z = x.intersection(y)
+print(z)
+
+#Keep the items that are not present in both sets:
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+x.symmetric_difference_update(y)
+print(x)
+
+#Return a set that contains all items from both sets, except items that are present in both:
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+z = x.symmetric_difference(y)
+print(z)
