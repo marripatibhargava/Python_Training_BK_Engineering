a810330268353a002f1ff40128bb20744d9ec455
Bhargava Krishna Marripati <marripatibhargava@gmail.com>
2022-02-21 12:09:35 +0530

    Initial Commit (Test) Updated till Python Numbers updated till place holders Updated till comprehension 591 tuples done 724 Sets Done 853 1040 for loops

--- main.py
+++ main.py
@@ -965,3 +965,77 @@ while i < 6:
   i += 1
 else:
   print("i is no longer less than 6")
+
+#For Loops
+#Print each fruit in a fruit list:
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+#Loop through the letters in the word "banana":
+
+for x in "banana":
+  print(x)
+
+#Exit the loop when x is "banana":
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
+#Exit the loop when x is "banana", but this time the break comes before the print:
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+#Do not print banana:
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+#Using the range() function:
+
+for x in range(6):
+  print(x)
+
+#Using the start parameter:
+
+for x in range(2, 6):
+  print(x)
+
+#increment the sequence with 3 (default is 1):
+for x in range(2, 30, 3):
+  print(x)
+
+#Print all numbers from 0 to 5, and print a message when the loop has ended:
+
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+#Break the loop when x is 3, and see what happens with the else block:
+
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+
+#Print each adjective for every fruit:
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
