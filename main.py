b5e79172dbc27c6cd87b55270b930cd5fad769aa
Bhargava Krishna Marripati <marripatibhargava@gmail.com>
2022-02-21 10:02:04 +0530

    839 While loop

--- main.py
+++ main.py
@@ -801,3 +801,38 @@ b = 200
 
 if b > a:
   pass
+
+#The while Loop
+
+#Print i as long as i is less than 6:
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+#Exit the loop when i is 3:
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+#Continue to the next iteration if i is 3:
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+#Print a message once the condition is false:
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
