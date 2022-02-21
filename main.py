374d5a670d626af047eb46c12d2df643416b64d3
Bhargava Krishna Marripati <marripatibhargava@gmail.com>
2022-02-21 09:46:35 +0530

    803 if..Else

--- main.py
+++ main.py
@@ -722,3 +722,82 @@ x = {"apple", "banana", "cherry"}
 y = {"google", "microsoft", "apple"}
 z = x.symmetric_difference(y)
 print(z)
+
+#If statement:
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+  
+#Elif: The elif keyword is pythons way of saying "if the previous conditions were not true, then try this condition".
+
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+  
+#Else The else keyword catches anything which isn't caught by the preceding conditions.
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+ 
+#You can also have an else without the elif:
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+  
+#One line if statement:
+if a > b: print("a is greater than b")
+  
+#Short Hand If ... Else
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+#One line if else statement, with 3 conditions:
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+#AND: Test if a is greater than b, AND if c is greater than a:
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+  
+#OR: Test if a is greater than b, OR if a is greater than c:
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")
+ 
+#Nested If: You can have if statements inside if statements, this is called nested if statements.
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
+    
+ #The pass Statement: if statements cannot be empty, but if you for some reason have an if statement with no content, put in the pass statement to avoid getting an error.
+a = 33
+b = 200
+
+if b > a:
+  pass
