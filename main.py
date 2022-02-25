9e4e4e96d5d8c39d3e24582ac3fb8747e9c28106
Bhargava Krishna Marripati <marripatibhargava@gmail.com>
2022-02-25 09:36:51 +0530

    Initial Commit (Test) Updated till Python Numbers updated till place holders Updated till comprehension 591 tuples done 724 Sets Done 853 1040 for loops 1074 1166 Objects

--- main.py
+++ main.py
@@ -1076,5 +1076,97 @@ cars = ["Ford", "Volvo", "BMW"]
 
 x = cars[0]
 
+#Return the number of elements in the cars array:
+
+x = len(cars)
+
+#Print each item in the cars array:
+
+for x in cars:
+  print(x)
+
+#Add one more element to the cars array:
+
+cars.append("Honda")
+
+#Delete the second element of the cars array:
+
+cars.pop(1)
+
+#Delete the element that has the value "Volvo":
+
+cars.remove("Volvo")
+
+#Class/ Objects
+
+#Create a class named MyClass, with a property named x:
+
+class MyClass:
+  x = 5
+#Now we can use the class named MyClass to create objects:
+#Create an object named p1, and print the value of x:
+
+p1 = MyClass()
+print(p1.x)
+
+#Create a class named Person, use the __init__() function to assign values for name and age:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+
+#Insert a function that prints a greeting, and execute it on the p1 object:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+#Use the words mysillyobject and abc instead of self:
+
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+#modify - Set the age of p1 to 40:
+
+p1.age = 40
+
+#Deleting object properties - Delete the age property from the p1 object:
+
+del p1.age
+
+#Delete Objects - Delete the p1 object:
+
+del p1
+
+#The pass Statement - class definitions cannot be empty, but if you for some reason have a class
+# definition with no content, put in the pass statement to avoid getting an error.
+
+class Person:
+  pass
+
+
+
+
 
 
